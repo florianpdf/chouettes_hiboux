688bbaa00015856b65a4f58ed2a5f2dccfa852d9
Aurore77 <aurore_david77@hotmail.fr>
2016-09-29 18:18:52 +0200

    rangement racine

---
+++ bijoux.php
@@ -0,0 +1,3 @@
+<?php include('includes/header_page.php');?>
+
+<?php include('includes/footer.php');?>
---
+++ doudous.php
@@ -0,0 +1,7 @@
+<?php include('includes/header_page.php');?>
+
+
+<?php include('includes/navbar.php');?>
+
+
+<?php include('includes/footer.php');?>
