d195d3c7362973edbecf23349a6787869800f813
Aurore77 <aurore_david77@hotmail.fr>
2016-10-24 15:59:07 +0200

    formulaire débuts essais mise en route

--- src/ChouettesBundle/Controller/DefaultController.php
+++ src/ChouettesBundle/Controller/DefaultController.php
@@ -92,33 +92,40 @@ class DefaultController extends Controller
 
 
 
-//    public function messageAction()
-//    {
-//        require_once '/lib/swift_required.php';
-//
-//        // Create the Transport
-//        $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 25);
-//
-//        // Create the Mailer using your created Transport
-//        $mailer = Swift_Mailer::newInstance($transport);
-//
-//        // Create a message
-//        $message = Swift_Message::newInstance('Wonderful Subject')
-//        ->setFrom(array('john@doe.com' => 'John Doe'))
-//        ->setTo(array('receiver@domain.org', 'other@domain.org' => 'A name'))
-//        ->setFirstName("Here is the sender's first name")
-//        ->setLastName("Here is the sender's last name")
-//        ->setEmail('Here is user email')
-//        ->setBody('Here is the message itself')
-//        ;
+    public function messageAction()
+    {
+        require_once '/lib/swift_required.php';
+
+        // Create the Transport
+        $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465);
+
+        // Create the Mailer using your created Transport
+        $mailer = Swift_Mailer::newInstance($transport);
+
+        // Create a message
+        $message = Swift_Message::newInstance('Wonderful Subject')
+        ->setFrom(array('bibouye77@gmail.com' => 'Bibouye'))
+        ->setTo(array('bibouye77@gmail.com'))
+        ->setFirstName("Here is the sender's first name")
+        ->setLastName("Here is the sender's last name")
+        ->setEmail('Here is user email')
+        ->setBody('Here is the message itself')
+        ;
 //
 //        // Send the message
-////        $result = $mailer->send($message);
-//        $status = $mailer->send($message);
-//        if($status) echo "Success!";
-//        else echo "Failure";
+      $result = $mailer->send($message);
+        $status = $mailer->send($message);
+        if($status)
+        {
+            echo "Success!";
+        }
+
+        else
+        {
+            echo "Failure";
+        }
 //    }
-//}
+}
 
     public function adminAction()
     {
