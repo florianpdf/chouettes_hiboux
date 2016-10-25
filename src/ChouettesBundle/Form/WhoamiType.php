27a01f24a047768cb31c65778345b7d51a22ca61
AF1890 <aylin.devillechenous@gmail.com>
2016-10-25 13:18:46 +0200

    ajout wysiwyg, upload image et cascade de whoami

--- src/ChouettesBundle/Form/WhoamiType.php
+++ src/ChouettesBundle/Form/WhoamiType.php
@@ -15,11 +15,12 @@ class WhoamiType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('text')
-            ->add('image')
+            //{#WYSIWYG#}
+            ->add('text', 'textarea', array('attr' => array('class' => 'tinymce', 'data-theme' => 'bbcode')))
+            ->add('image', ImageType::class)
         ;
     }
-    
+
     /**
      * @param OptionsResolver $resolver
      */
