d57514d0eac4e0e629eefc8746cc15d029a891d8
Anthooo <anthony.menet@gmail.com>
2016-11-13 11:13:08 +0100

    ajout SEO

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new ChouettesBundle\ChouettesBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Lexik\Bundle\MaintenanceBundle\LexikMaintenanceBundle(),
+            new Sonata\SeoBundle\SonataSeoBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
